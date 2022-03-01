7239bd97443b01a08fb295cfd4792e09e05e2107
kejubayer <khondokereftakharjubayer@gmail.com>
2022-03-02 01:08:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
+import Student from "./pages/Student";
+import Addstudent from "./pages/Addstudent";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Student} />
+                <Route path="/add-student" component={Addstudent} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
